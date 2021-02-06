4ba977f0440ce395a3ab92cb142491189f016b06
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2021-02-06 14:28:08 +0330

    Add Phase01-Search folder

---
+++ Phase01-Search/src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    private static InvertedIndex invertedIndex;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        while (!scanner.nextLine().equals("3")){
+//            System.out.println("1. Add document");
+//            System.out.println("2. Search");
+//            System.out.println("3. Exit");
+//            String input=scanner.nextLine();
+//            if(input.equals("1")){
+//
+//            }else if(input.equals("2")){
+//
+//            }else{
+//                System.out.println("Invalid Command");
+//            }
+//        }
+        FileReader fileReader = new FileReader("C:/Users/Amkam/Desktop/Folder");
+        invertedIndex = new InvertedIndex();
+        for (File allFile : fileReader.getAllFiles()) {
+            try {
+                separate(allFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("What is your search word?");
+        String searchWord = scanner.nextLine();
+        if (invertedIndex.search(searchWord) != null) {
+            System.out.println(invertedIndex.search(searchWord).toString());
+        } else {
+            System.out.println("Not Found");
+        }
+
+    }
+
+    public static void separate(File file) throws FileNotFoundException {
+        Scanner reader = new Scanner(file);
+        while (reader.hasNextLine()) {
+            String[] word = reader.nextLine().split(" ");
+            for (String s : word) {
+                invertedIndex.addWord(s, file.getName());
+            }
+        }
+
+    }
+}
