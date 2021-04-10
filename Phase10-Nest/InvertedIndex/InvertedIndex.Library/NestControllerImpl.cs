e0759bcf68697fe1a93d1bdb32131c76f3ccaadc
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2021-04-10 16:37:59 +0430

    connection done

--- Phase10-Nest/InvertedIndex/InvertedIndex.Library/NestControllerImpl.cs
+++ Phase10-Nest/InvertedIndex/InvertedIndex.Library/NestControllerImpl.cs
@@ -1,22 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Nest;
 
 namespace InvertedIndex.Library
 {
     public class NestControllerImpl
     {
+        private ElasticClient _client;
+
         public IElasticClient InitialiseConnectingToElasticClient()
         {
             var uri = new Uri("http://localhost:9200");
             var connectionSetting = new ConnectionSettings(uri);
             connectionSetting.EnableDebugMode();
             var client = new ElasticClient(connectionSetting);
+            _client = client;
             return client;
         }
 
-        public void SaveWordsInDatabase()
+        public async Task SaveWordsInProgramDatabaseToElastic(List<Word> words)
         {
-            
+            foreach (var word in words)
+            {
+                var response = await _client.IndexAsync<Word>(word, descriptor => descriptor
+                    .Index("inverted-index")).ConfigureAwait(false);
+            }
         }
     }
 }
--- Phase10-Nest/InvertedIndex/InvertedIndex/Program.cs
+++ Phase10-Nest/InvertedIndex/InvertedIndex/Program.cs
@@ -1,22 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks;
 using InvertedIndex.Library;
 
 namespace Project
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             FilesDatabaseControllerImpl docsController = new FilesDatabaseControllerImpl();
             Console.WriteLine("Enter the directory name:");
             docsController.InitialiseReadAndSavingFiles(Console.ReadLine());
             var wordsController = new WordsDatabaseControllerImpl();
             wordsController.InitialiseUpdateDatabase(docsController.GetAllDocs());
-            foreach (var word in wordsController.AllWords)
-            {
-                Console.WriteLine(word.Name);
-                Console.WriteLine(word.Docs.Count);
-            }
+
+            var nestController = new NestControllerImpl();
+            var client = nestController.InitialiseConnectingToElasticClient();
+            nestController.SaveWordsInProgramDatabaseToElastic(wordsController.AllWords);
+
+            // foreach (var word in wordsController.AllWords)
+            // {
+            //     var response = await client.IndexAsync<Word>(word, descriptor => descriptor
+            //         .Index("inverted-index")).ConfigureAwait(false);
+            // }
+
+            // foreach (var word in wordsController.AllWords)
+            // {
+            //     Console.WriteLine(word.Name);
+            //     Console.WriteLine(word.Docs.Count);
+            // }
         }
     }
 }
